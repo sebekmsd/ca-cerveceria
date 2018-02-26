fa6e5c668f52226fef7105ba1e4adabc30ce240a
msilva@arkhotech.com <msilva@arkhotech.com>
2018-02-26 17:22:25 -0300

    Modificacióm: reles dinámicos

--- io_iface.js
+++ io_iface.js
@@ -3,6 +3,11 @@ console.log('*** inicializando aplicacion');
 var tempMax = ( process.env.MAX_TEMP != null ) ? process.env.MAX_TEMP : 20 ;
 var tempMin = ( process.env.MIN_TEMP != null ) ? process.env.MIN_TEMP : 15 ; 
 
+var relay_pins = { 18,0,1,6,7};
+
+const MAX_NUM_RELAY = 3;
+
+var relays= Array();
 
 var W1Temp = require('w1temp');
 var gpio = require("omega_gpio");
@@ -43,30 +48,49 @@ OmegaIFace.prototype.temperature = ()=>{
 }
 
 
-OmegaIFace.prototype.initRelay=()=>{
-   console.log('iniciando sistema de relay');
-   var Relay = gpio.Relay;
-   R1 = new Relay(18);	
-   R1.off();
-   R1.on();
+OmegaIFace.prototype.initRelay=(num_relay)=>{
+  console.log('iniciando sistema de relay');
+  if(num_relay !== undefined ){
+
+  }else{
+
+  }
+  for( i = 0 ;  i < num_relay && i < MAX_NUM_RELAY ; i++ ){
+    console.log('Configurando pin ' + relay_pins[i]);
+    relays.push(gpio.Relay);
+    relays[i] = new Relay(relay_pins[i]);
+    relays[i].off();
+    relays[i].on();
+  }
+  
 }
 
 OmegaIFace.prototype.destroyRelay = ()=>{
-  if ( R1 != null && R1 != undefined ){ 
-     R1.destroy();
+  for( i = 0 ; i < relays.length; i++ ){
+
+    if ( relays[i] != null && relays[i] != undefined ){ 
+      relays[i].destroy();
+    }
   }
+  
 }
 
 
 
 OmegaIFace.prototype.openRelay = (releId) => {
-  R1.on();
+  if( releId >= relays.length ){
+    throw new Exception('El identificador no existe');
+  }
+  relays[releId-1].on();  //En base cero
   console.log('Abriendo rele ' + releId);
 }
 
 OmegaIFace.prototype.closeRelay = (releId) => {
-  console.log('cerrando rele');
-  R1.off();
+  if( releId >= relays.length ){
+    throw new Exception('El identificador no existe');
+  }
+  relays[releId-1].off();  //En base cero
+  console.log('Abriendo rele ' + releId);
 }
 
 
